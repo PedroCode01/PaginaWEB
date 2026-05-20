066d390745f6a38ef9310f27338e26c4a8745254
PedroCode01 <pedronaldi1000@gmail.com>
2026-05-20 16:53:36 -0400

    commit

--- novo_site/usuarios/views.py
+++ novo_site/usuarios/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 # Create your views here.
+
+def home_view(request):
+    return HttpResponse("<p>Olá, sou o Pedro e essa é a página de login do usuário!</p")
 
 def login(request):
     return render(request, 'usuarios/login.hmtl')
