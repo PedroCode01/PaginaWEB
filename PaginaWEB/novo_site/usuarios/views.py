f1d347f05242f4fbfd8772d92e152ae56a0edb5f
PedroCode01 <65756494+PedroCode01@users.noreply.github.com>
2026-06-08 16:56:43 -0400

    Add files via upload

---
+++ PaginaWEB/novo_site/usuarios/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render # Usada para renderizar templates HTML.
+from django.http import HttpResponse
+
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+
+def base(request):
+    if request.method == 'GET':
+        return render(request, 'usuarios/base.html')
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'usuarios/login.html')
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
+        user = authenticate(request, username=email, password=senha)
+        if user is not None:
+            login_django(request, user)
+            return HttpResponse('Login realizado com sucesso!')
+        else:
+            return HttpResponse('Credenciais inválidas. Tente novamente.')
+def home_view(request):
+    return HttpResponse("Olá, sou o Pedro e essa é a página de login do usuário!")
+
+def cadastro(request):
+    if request.method == 'GET':
+        return render(request, 'usuarios/cadastro.html')
+    
+    username = request.POST.get('email')
+    email = request.POST.get('email')
+    password = request.POST.get('senha')
+    first_name = request.POST.get('nome')
+
+    user = User.objects.filter(username=username).first()
+
+    if user:
+        return HttpResponse('Usuário já existe. Por favor, escolha outro email.')
+    else:
+        user = User.objects.create_user(
+        username=username,
+        email=email,
+        password=password, 
+        first_name=first_name
+        )
+        return render(request, 'cadastro.html', {'sucesso': True})
